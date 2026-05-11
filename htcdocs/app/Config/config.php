d51a57561b89574301a871836c88ba8287de5d00
Hoshi12345 <62142732+Hoshi12345@users.noreply.github.com>
2026-05-11 17:27:27 -0400

    omitir .env en git ignore y creacion archivo deploy.yml, config.php

---
+++ htcdocs/app/Config/config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php – Configuración de base de datos (DESARROLLO LOCAL)
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'elfaro_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
+?>
