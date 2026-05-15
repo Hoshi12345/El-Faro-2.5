16e1ce3d69395594cfc4b4a2ed452c9a705d62e2
Hoshi12345 <62142732+Hoshi12345@users.noreply.github.com>
2026-05-14 21:21:57 -0400

    modificado home.php agregado el boton toggle dark mode

--- htdocs/app/Views/home.php
+++ htdocs/app/Views/home.php
@@ -35,6 +35,7 @@
     <h1 class="title is-4 has-text-black mt-2">Bienvenidos a periódico El Faro</h1>
     <div id="reloj-movil" class="tag is-dark is-medium mt-2">00:00:00</div>
   </div>
+  <div><button class="button toggle-dark mode is-light mt-2" onclick="myFunction()">Toggle Dark Mode</button></div>
 </header>
 
 <header class="has-background-info py-4 is-hidden-mobile">
