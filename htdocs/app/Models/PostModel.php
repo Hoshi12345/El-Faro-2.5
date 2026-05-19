c6301f2292fbc009ceb018312477ae96e23c3ebf
Hoshi12345 <62142732+Hoshi12345@users.noreply.github.com>
2026-05-19 19:42:42 -0400

    cambio en carpeta models en archivo posts.phph agregada images

--- htdocs/app/Models/PostModel.php
+++ htdocs/app/Models/PostModel.php
@@ -5,6 +5,6 @@ class PostModel extends Model
 {
     protected $table      = 'posts';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['title', 'content', 'categoria', 'fuente', 'seccion'];
+    protected $allowedFields = ['title', 'content', 'categoria', 'fuente', 'seccion','imagen'];
     protected $useTimestamps = false;  // Usaremos el TIMESTAMP de MySQL
 }
