1fece87c2c210d5067ac79e24ef1076b64cda1af
Hoshi12345 <62142732+Hoshi12345@users.noreply.github.com>
2026-05-19 21:57:15 -0400

    modificacion agregado get articulos a archivov routes.php

--- htdocs/app/Config/Routes.php
+++ htdocs/app/Config/Routes.php
@@ -14,6 +14,7 @@ $routes->post('articulo/store', 'Articulo::store');
 $routes->get('contacto', 'Contacto::index');
 $routes->post('contacto/enviar', 'Contacto::enviar');
 $routes->get('admin/usuarios', 'Admin::usuarios');
+$routes->get('articulo/getArticulo/(:num)', 'Articulo::getArticulo/$1');
 
 $routes->get('test-contacto', function() {
     return 'Ruta de prueba OK';
