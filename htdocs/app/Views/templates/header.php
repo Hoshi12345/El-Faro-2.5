2412a629b08f71f8babf7e6e82985669e16a241f
Hoshi12345 <62142732+Hoshi12345@users.noreply.github.com>
2026-05-19 20:49:23 -0400

    css versionado

--- htdocs/app/Views/templates/header.php
+++ htdocs/app/Views/templates/header.php
@@ -14,7 +14,7 @@
     <title>Periódico El Faro</title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
-    <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
+   <link rel="stylesheet" href="<?= base_url('css/styles.css?v=1.935') ?>">
 </head>
 <body>
 
